4f76e40297d9a1fe0bbae69a3fc722ce36e6cda7
davis <zanxiaofeng@163.com>
2023-04-15 10:41:49 +0800

    update

---
+++ gateway-service/src/main/java/zxf/springboot/gatewayservice/filter/ClearHeaderFilter.java
@@ -0,0 +1,25 @@
+package zxf.springboot.gatewayservice.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class ClearHeaderFilter implements GlobalFilter, Ordered {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        exchange.getResponse().beforeCommit(() -> {
+            exchange.getResponse().getHeaders().remove("Un-Used-Key");
+            return Mono.empty();
+        });
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return -200;
+    }
+}
